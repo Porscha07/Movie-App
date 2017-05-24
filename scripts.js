d85f0f1f74bd5b21fb35c940beff782c2c17b170
Porscha07 <porscha.uga@gmail.com>
2017-05-24 17:46:33 -0400

    html background and nav bar improvement

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@ $('document').ready(function(){
 	const imageBaseUrl = 'http://image.tmdb.org/t/p/';
 
 	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key=' + apiKey
+
+	const tvUrl = apiBaseUrl + '/tv/{tv_id}=<<api_key>>&language=en-US' + apiKey //tv url for search selection
 	// console.log(nowPlayingUrl);
 // ********make ajax request to the nowPlayingUrl****
 	$.getJSON(nowPlayingUrl,(nowPlayingData)=>{ // on load
@@ -25,10 +27,29 @@ $('document').ready(function(){
 				$("#myModal").modal();
 			});
 		});
-});
+
+	// ********make ajax request to the tvUrl********
+		// 	$.getJSON(nowPlayingUrl,(nowPlayingData)=>{ // on load
+		// // console.log(nowPlayingData);
+		// var nowPlayingHTML= getHTML(nowPlayingData);
+		// $('#movie-grid').html(nowPlayingHTML);
+		// $('.movie-poster').click(function(){
+
+		// 	var thisMovieId = $(this).attr('movie-id');
+		// 	console.log(thisMovieId);
+		// 	var thisMovieUrl = `${apiBaseUrl}/movie/${thisMovieId}?api_key=${apiKey}`;
+		// 	$.getJSON(thisMovieUrl,(thisMovieData)=>{
+		// 		console.log(thisMovieData);
+		// 		$('#myModalLabel').html(thisMovieData.title);
+		// 		// Open teh modal
+		// 		$("#myModal").modal();
+		// 	});
+		// });
+
+// **********************Submit Handler******************************************
 		
 	$('#movie-form').submit((event)=>{ // want to use an event listener on your form
-		console.log("gadgds")
+		
 		//Don't submit Form, JS will handle
 		event.preventDefault();
 		var userInput = $('#search-input').val(); // only inputs have values!
@@ -43,7 +64,32 @@ $('document').ready(function(){
 			var searchMovieHTML = getHTML(searchMovieData); // updating the DOM/ utility function
 			$('#movie-grid').html(searchMovieHTML); 
 		});
+		//*********************Submit for TV**************************
+
+		$('#tv-form').submit((event)=>{ // want to use an event listener on your form
+		
+		//Don't submit Form, JS will handle
+		event.preventDefault();
+		var userInput = $('#search-input').val(); // only inputs have values!
+		$('search-input').val(''); // will set code back to nothing ( after you insert something)
+		var safeUserInput = encodeURI(userInput);
+		var searchUrl = apiBaseUrl + '/search/movie?query='+ safeUserInput + '&api_key=' + apiKey;
+		console.log(searchUrl);
+
+
+		//the code below will only happen if they submit the form
+		// $.getJSON(searchUrl,(searchMovieData)=>{
+		// 	var searchMovieHTML = getHTML(searchMovieData); // updating the DOM/ utility function
+		// 	$('#movie-grid').html(searchMovieHTML); 
+		// });
+		$.getJSON(searchUrl,(tvData)=>{
+			var searchTVHTML = getHTML(tvData); // updating the DOM/ utility function
+			$('#movie-grid').html(searchtvHTML); 
+		});
+
 	});
+
+	// ***************************** Search Results**********************************************
 		function getHTML(nowPlayingData){// change
 			var nowPlayingHTML= ' ';
 			//loop for all json data retrieved from request.
@@ -56,14 +102,18 @@ $('document').ready(function(){
 			return nowPlayingHTML
 		}
 
+		// ********************************TV Results********************************************
+		function tvHTML(tvData){// change
+			var tvHTML= ' ';
+			//loop for all json data retrieved from request.
+			for(let i = 0; i < tvData.results.length; i++){
+				var posterURL = imageBaseUrl + 'w300'+ tvData.results[i].poster_path;
+				tvHTML += '<div class="col-sm-6 col-md-3">';
+					tvHTML += `<img src="${posterURL}">`;
+				tvHTML += `</div>`;
+			}
+			return tvHTML
+		}
+	});	
 
-	// $('#dropdown-menu').on('show.bs.dropdown',()=>{
-
-	// 	apiBaseUrl = []
-	// 	suspense[];
-	// 	for(let i = 0; i < nowPlayingData.results.length;i++){
-	// 		if()
-	// 	}
-
-	// });
 });
